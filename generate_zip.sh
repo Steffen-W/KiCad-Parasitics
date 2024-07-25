f5bf42995be962cbdb0871f521f269be376ba953
Steffen Wittemeier <uni@steffenwittemeier.de>
2024-07-25 11:10:20 +0200

    update generate_zip.sh

--- generate_zip.sh
+++ generate_zip.sh
@@ -2,7 +2,7 @@
 
 rm KiCad-Parasitics.zip
 mv metadata.json metadata_.json
-jq --arg today "$(date +%Y-%m-%d)" '.versions[0].version |= $today' metadata_.json > metadata.json
+jq --arg today "$(date +%Y.%m.%d)" '.versions[0].version |= $today' metadata_.json > metadata.json
 
 git ls-files  -- 'metadata.json' 'resources*.png' 'plugins*.png' 'plugins*.py' | xargs zip KiCad-Parasitics.zip
 mv metadata_.json metadata.json
